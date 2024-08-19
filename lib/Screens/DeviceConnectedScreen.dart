fc0873c40f0e0ad3fd10675e8ae78c198f6ebf53
immp-32 <immp3353@gmail.com>
2024-08-19 11:24:08 +0545

    voice controller implementing

--- lib/Screens/DeviceConnectedScreen.dart
+++ lib/Screens/DeviceConnectedScreen.dart
@@ -5,35 +5,73 @@ import 'dart:convert';
 import 'dart:typed_data';
 import 'BluetoothListScreen.dart';
 
-class DeviceConnectedScreen extends StatelessWidget {
+class DeviceConnectedScreen extends StatefulWidget {
   final BluetoothDevice device;
 
   const DeviceConnectedScreen({super.key, required this.device});
 
   @override
+  State<DeviceConnectedScreen> createState() => _DeviceConnectedScreenState();
+}
+
+class _DeviceConnectedScreenState extends State<DeviceConnectedScreen> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Connected to ${device.name ?? "Unknown Device"}'),
+        title: Text('Connected to ${widget.device.name ?? "Unknown Device"}'),
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(20),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () async {
-                await _sendMessageToBluetooth('1'); // Send '1' to turn on the LED
-              },
-              child: Text('ON'),
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                await _sendMessageToBluetooth('0'); // Send '0' to turn off the LED
-              },
-              child: Text('OFF'),
-            ),
-          ],
-        ),
+          crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Card(
+                elevation: 10,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Column(
+                    children: [
+                      ElevatedButton(
+                        onPressed: () async {
+                          await _sendMessageToBluetooth('1'); // Send '1' to turn on the LED
+                        },
+                        child: Text('ON'),
+                      ),
+                      ElevatedButton(
+                        onPressed: () async {
+                          await _sendMessageToBluetooth('0'); // Send '0' to turn off the LED
+                        },
+                        child: Text('OFF'),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 30,),
+              Container(
+                width: 120,
+                height: 120,
+                child: Card(
+                  elevation: 5,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Container(
+                    child: GestureDetector(
+                      onTap: (){
+
+                      },
+                        child: Image.asset('assets/icons/microphone.png')),
+                  ),
+                ),
+              )
+
+            ],
+          ),
       ),
     );
   }
