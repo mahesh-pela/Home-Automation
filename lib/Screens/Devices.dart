19246cd364413bb0a79f50e86ba0e84200626f6f
immp-32 <immp3353@gmail.com>
2024-08-24 22:48:04 +0545

    adding devices

---
+++ lib/Screens/Devices.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Devices extends StatelessWidget {
+  const Devices({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
