fcc80b3ff45d312cab66734b2d727ab9ebfe8a05
immp-32 <immp3353@gmail.com>
2024-09-15 12:55:50 +0545

    login screen modified

--- lib/Screens/loginScreen.dart
+++ lib/Screens/loginScreen.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:home_automation/Screens/signupScreen.dart';
-import '../constants/textFieldDecoration.dart';
 
 class Loginscreen extends StatefulWidget {
   const Loginscreen({super.key});
@@ -17,7 +16,7 @@ class _LoginscreenState extends State<Loginscreen> {
   TextEditingController txtPassController = TextEditingController();
   var _obscureText = true;
 
-  void showHidePassword(){
+  void showHidePassword() {
     setState(() {
       _obscureText = !_obscureText;
     });
@@ -27,30 +26,41 @@ class _LoginscreenState extends State<Loginscreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: CupertinoColors.white,
-      appBar: AppBar(backgroundColor: CupertinoColors.white,),
+      appBar: AppBar(
+        backgroundColor: CupertinoColors.white,
+      ),
       body: Padding(
         padding: const EdgeInsets.all(15),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            SizedBox(height: 30,),
-            Text('Login', style: GoogleFonts.poppins(fontSize: 26, fontWeight: FontWeight.w600),),
-            SizedBox(height: 20,),
+            SizedBox(
+              height: 30,
+            ),
+            Text(
+              'Login',
+              style: GoogleFonts.poppins(
+                  fontSize: 26, fontWeight: FontWeight.w600),
+            ),
+            SizedBox(
+              height: 20,
+            ),
 
             ///---email address textfield---///
             TextField(
               controller: txtEmailController,
               keyboardType: TextInputType.emailAddress,
               decoration: InputDecoration(
-                hintText: 'Email Address',
-                hintStyle: TextStyle(color: Colors.grey.shade600),
-                prefixIcon: Icon(Icons.email),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12),
-                )
-              ),
+                  hintText: 'Email Address',
+                  hintStyle: TextStyle(color: Colors.grey.shade600),
+                  prefixIcon: Icon(Icons.email),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  )),
             ),
-            SizedBox(height: 10,),
+            SizedBox(
+              height: 10,
+            ),
 
             ///---password textfield----///
             TextField(
@@ -62,86 +72,123 @@ class _LoginscreenState extends State<Loginscreen> {
                   hintStyle: TextStyle(color: Colors.grey.shade600),
                   prefixIcon: Icon(Icons.password),
                   suffixIcon: IconButton(
-                    onPressed: showHidePassword,
-                    icon: Icon(_obscureText ? Icons.visibility_off : Icons.visibility)
-                  ),
+                      onPressed: showHidePassword,
+                      icon: Icon(_obscureText
+                          ? Icons.visibility_off
+                          : Icons.visibility)),
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(12),
-                  )
-              ),
+                  )),
             ),
-            SizedBox(height: 5,),
+            SizedBox(
+              height: 5,
+            ),
 
             ///----forgot password---///
             Row(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                Text('Forgot Password?', style: TextStyle(color: Colors.grey.shade600, fontWeight: FontWeight.w500),)
+                Text(
+                  'Forgot Password?',
+                  style: TextStyle(
+                      color: Colors.grey.shade600, fontWeight: FontWeight.w500),
+                )
               ],
             ),
-            SizedBox(height: 15,),
+            SizedBox(
+              height: 15,
+            ),
 
             ///----Login Button-----///
             Container(
               width: double.infinity,
-              height: 50,
+              height: 45,
               child: ElevatedButton(
-                  onPressed: (){
-                  },
-                  child: Text('Login', style: TextStyle(color: CupertinoColors.white, fontSize: 21, fontWeight: FontWeight.w600),),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: CupertinoColors.activeBlue,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20)
-                  )
+                onPressed: () {},
+                child: Text(
+                  'Sign In',
+                  style: TextStyle(
+                      color: CupertinoColors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600),
                 ),
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: CupertinoColors.activeBlue,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20))),
               ),
             ),
-            SizedBox(height: 15,),
-
-            Center(child: Text("Don't Have an Account? Create Now")),
-            SizedBox(height: 15,),
-
-            Center(child: Text('Or Sign in With', style: TextStyle(color: Colors.grey.shade600),)),
-            SizedBox(height: 15,),
+            SizedBox(
+              height: 15,
+            ),
 
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Container(
-                  width: 160,
-                  height: 55,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                    border: Border.all(
-                        width: 1.5,
-                        color: Colors.grey.shade300
+                Text("Don't Have an Account? ", style: TextStyle(fontWeight: FontWeight.w500, fontSize: 15),),
+                InkWell(
+                  onTap: (){
+                    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=> Signupscreen()));
+                  },
+                    child: Text('Create Now', style: TextStyle(color: Colors.red, fontWeight: FontWeight.w600),)
+                )
+              ],
+            ),
+            SizedBox(height: 10,),
+
+            Center(
+                child: Text(
+              'Or',
+              style: TextStyle(color: Colors.grey.shade600),
+            )),
+            SizedBox(
+              height: 15,
+            ),
+
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    width: double.infinity,
+                    height: 55,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      border: Border.all(width: 1.5, color: Colors.grey.shade300),
+                    ),
+
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        IconButton(
+                          onPressed: () {},
+                          icon: Image.asset('assets/icons/google logo.png'),
+                        ),
+                        SizedBox(width: 10,),
+                        Text('Sign In With Google', style: TextStyle(fontSize: 15, fontWeight: FontWeight.w600),)
+                      ],
                     ),
                   ),
-                  child: IconButton(
-                    onPressed: (){},
-                    icon: Image.asset('assets/icons/google logo.png'),
-                  ),
                 ),
-
-                // Container(
-                //   width: 155,
-                //   height: 55,
-                //   decoration: BoxDecoration(
-                //     borderRadius: BorderRadius.circular(15),
-                //     border: Border.all(
-                //         width: 1,
-                //       color: Colors.grey.shade500
-                //     )
-                //   ),
-                //
-                //   child: IconButton(
-                //     onPressed: (){},
-                //     icon: Image.asset('assets/icons/facebook logo.png'),
-                //   ),
-                // )
               ],
-            )
+            ),
+
+
+            // Container(
+            //   width: 155,
+            //   height: 55,
+            //   decoration: BoxDecoration(
+            //     borderRadius: BorderRadius.circular(15),
+            //     border: Border.all(
+            //         width: 1,
+            //       color: Colors.grey.shade500
+            //     )
+            //   ),
+            //
+            //   child: IconButton(
+            //     onPressed: (){},
+            //     icon: Image.asset('assets/icons/facebook logo.png'),
+            //   ),
+            // )
           ],
         ),
       ),
